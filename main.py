43b8a1a6cc7e7bf97a24a7a2bf6253ccd6fbc69d
Rakha R. Putra <rakharamadhia@gmail.com>
2023-03-03 09:45:38 +0000

    command hint for the answer

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def game():
   print("Welcome to the Number Guessing Game!")
   print("I'm thinking of a number between 1 and 100.")
   answer = randint(1, 100)
+  # hint for the right answer
   # print(f"Pssst, the correct answer is {answer}") 
 
   turns = set_difficulty()
